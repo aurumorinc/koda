d4f12f12c6d58dc46a8bb6f5e91b2c20c5d279ef
Aryan Singh <aquiveal@gmail.com>
2026-06-27 22:37:59 +0530

    chore: remove page and site crawling modules
    Delete the scraping and crawling implementation files and schemas as they are no longer required in this repository.

--- packages/koda/src/koda/modules/page/__init__.py
+++
@@ -1,4 +0,0 @@
-from koda.modules.page.schema import (Action, ScrapeRequest, ScrapeResponse,)
-from koda.modules.page.service import (ScrapeJob,)
-
-__all__ = ['Action', 'ScrapeRequest', 'ScrapeResponse', 'ScrapeJob']
--- packages/koda/src/koda/modules/site/__init__.py
+++
@@ -1,4 +0,0 @@
-from koda.modules.site.schema import (CrawlRequest, CrawlResponse, ScrapeOptions,)
-from koda.modules.site.service import (CrawlJob,)
-
-__all__ = ['CrawlRequest', 'CrawlResponse', 'ScrapeOptions', 'CrawlJob']
